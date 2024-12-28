91ed80304d814f5a8068288a796eb61167a1ca8c
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2024-12-28 16:45:06 +0000

    Create Source.cs

---
+++ Source.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "cheat";
+        Console.WriteLine("This is a blank console application named 'cheat'.");
+        Console.ReadLine();
+    }
+}
